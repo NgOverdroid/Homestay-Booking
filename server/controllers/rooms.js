27eb150642ba40b2b6926991cfcb672d9cc95b22
Duc Nghia <21521180@gm.uit.edu.vn>
2024-10-09 16:16:17 +0700

    update rooms.js

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -6,9 +6,7 @@ router.get("/room:room_id", async (req, res) => {
     try {
         const room = await getRoom(req.params.room_id);
         if(room)
-            res.json({
-                "room": room,
-            });
+            res.send(room);
         else
             res.status(400).json({message: "room not found"});
     } catch (error) {
@@ -20,10 +18,7 @@ router.get("/room:room_id", async (req, res) => {
 router.post("/room:room_id/contract", async (req, res) => {
     try {
         const contract = await createContract();
-        res.json({
-            "contract_created": true,
-            "contract": contract
-        });
+        res.json(contract);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "An error occurred while creating a contract" });
